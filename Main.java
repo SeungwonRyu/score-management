5418403105f02a7e75b408aa79ba0ae4852f84c2
KimTaeseongHome <pdthswown@gmail.com>
2021-04-08 11:31:34 +0900

    입력받아야 하는 점수 갯수정하는 소스 추가

--- Main.java
+++ Main.java
@@ -6,17 +6,23 @@ class Main {
     Scanner scan = new Scanner(System.in);
 
     //점수를 넣을 배열생성
-    int[] point = new int[3];
+    int num;
+
+    System.out.print("몇개의 성적을 입력합니까?");
+    num = scan.nextInt();
+
+    //배열 선언
+    int[] point = new int[num];
     int newPoint;
 
-    for(int i=0;i<3;i++){
-      System.out.print("i번째 성적 입력 : ");
+    for(int i=0;i<num;i++){
+      System.out.print((i+1)+"번째 성적 입력 : ");
       point[i] = scan.nextInt();
     }
 
     Arrays.sort(point);
 
-    for(int i=0;i<3;i++){
+    for(int i=0;i<num;i++){
       System.out.print(point[i]+"\n");
     }
   }
